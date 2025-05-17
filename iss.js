8b11b5fabab56bfdc1a49ea88e64e9db1288bc8f
chisu-kawahara <chisu.kawahara@gmail.com>
2025-05-17 15:59:04 -0700

    checking

--- iss.js
+++ iss.js
@@ -1,14 +1,25 @@
+const needle = require('needle');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
 const fetchMyIP = function(callback) {
-  const url = 'https://api.ipify.org?format=json';
-  needle.get(url, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
+  needle.get('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
       return;
     }
-    callback(null, body.ip);
+
+    const ip = body.ip;
+    callback(null, ip);
   });
 };
+
+module.exports = { fetchMyIP };
