1fc4517dcefeb9fc2c587576f15d56050106c8ac
chisu-kawahara <chisu.kawahara@gmail.com>
2025-05-17 16:19:39 -0700

    define fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 // The code below is temporary and can be commented out.
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
-const { nextISSTimesForMyLocation } = require('./iss');
-
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  console.log('It worked! Returned IP:' , ip);
-});
-
-const { nextISSTimesForMyLocation } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
